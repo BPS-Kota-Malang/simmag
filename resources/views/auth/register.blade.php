e03cf4ed0f5a99f356db1aa1d30c266a4ef74bb9
Andi Wicaksono Nugroho <75154388+Andiwn121@users.noreply.github.com>
2023-09-14 14:05:04 +0700

    update slicing register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,60 +1,119 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="http://schema.org/WebPage">
 
-        <x-jet-validation-errors class="mb-4" />
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+    <title>
+        Register Simmag
+    </title>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link id="pagestyle" href="../assets/css/soft-design-system.css?v=1.0.9" rel="stylesheet" />
+    <!-- Nepcha Analytics (nepcha.com) -->
+    <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
+    <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+</head>
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+<body class="sign-in-illustration">
+    <!-- Navbar -->
+    <section>
+        <div class="page-header min-vh-100">
+            <div class="container">
+                <div class="row">
+                    {{-- <x-jet-validation-errors class="mb-4" /> --}}
+                    <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
+                        <div class="card card-plain">
+                            <div class="card-header pb-0 text-left">
+                                <h4 class="font-weight-bolder">Register</h4>
+                                <p class="mb-0">Enter your email and password to register</p>
+                            </div>
+                            <div class="card-body">
+                                <form method="POST" action="{{ route('register') }}">
+                                    @csrf
+                                    <label>{{ __('Name') }}</label>
+                                    <div class="mb-3">
+                                        <input type="text" id="name" class="form-control" placeholder="Name"
+                                            aria-label="Name" name="name" :value="old('name')" required autofocus
+                                            autocomplete="name">
+                                    </div>
+                                    <label>{{ __('Email') }}</label>
+                                    <div class="mb-3">
+                                        <input type="email" id="email" class="form-control" placeholder="Email"
+                                            aria-label="Email" name="email" :value="old('email')" required>
+                                    </div>
+                                    <label>{{ __('Password') }}</label>
+                                    <div class="mb-3">
+                                        <input type="password" id="password" class="form-control"
+                                            placeholder="Password" aria-label="Password" name="password" required
+                                            autocomplete="new-password">
+                                    </div>
+                                    <label>{{ __('Confirm Password') }}</label>
+                                    <div class="mb-3">
+                                        <input type="password" id="password_confirmation" class="form-control"
+                                            placeholder="Confirm Password" aria-label="Confirm Password"
+                                            name="password_confirmation" required autocomplete="new-password">
+                                    </div>
 
-            <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-jet-label for="terms">
-                        <div class="flex items-center">
-                            <x-jet-checkbox name="terms" id="terms"/>
-
-                            <div class="ml-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
-                                ]) !!}
+                                    <div class="text-center">
+                                        <x-jet-button
+                                            class="btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0 pt-3 pb-3">
+                                            <span style="font-size: 16px">
+                                                {{ __('Register') }}
+                                            </span>
+                                        </x-jet-button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="card-footer text-center pt-0 px-sm-4 px-1">
+                                <p class="mb-4 mx-auto">
+                                    {{ __('Already registered?') }}
+                                    <a href="{{ route('login') }}"
+                                        class="text-primary text-gradient font-weight-bold">Log in</a>
+                                </p>
                             </div>
                         </div>
-                    </x-jet-label>
+                    </div>
+                    <div
+                        class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
+                        <div
+                            class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center">
+                            <img src="../../assets/img/shapes/pattern-lines.svg" alt="pattern-lines"
+                                class="position-absolute opacity-4 start-0">
+                            <div class="position-relative">
+                                <img class="max-width-500 w-100 position-relative z-index-2"
+                                    src="../../assets/img/illustrations/chat.png" alt="image">
+                            </div>
+                            <h4 class="mt-5 text-white font-weight-bolder">Your journey starts here</h4>
+                            <p class="text-white">Just as it takes a company to sustain a product, it takes a community
+                                to sustain a protocol.</p>
+                        </div>
+                    </div>
                 </div>
-            @endif
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-jet-button class="ml-4">
-                    {{ __('Register') }}
-                </x-jet-button>
             </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+        </div>
+        </div>
+
+    </section>
+    <!--   Core JS Files   -->
+    <script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
+    <script src="../assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+    <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <!--  Plugin for Parallax, full documentation here: https://github.com/wagerfield/parallax  -->
+    <script src="../assets/js/plugins/parallax.min.js"></script>
+    <!-- Control Center for Soft UI Kit: parallax effects, scripts for the example pages etc -->
+    <!--  Google Maps Plugin    -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
+    <script src="../assets/js/soft-design-system.min.js?v=1.0.9" type="text/javascript"></script>
+</body>
+
+</html>
