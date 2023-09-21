a8565e37d5202fb01b1be198e87de3bfca5b10aa
Andi Wicaksono Nugroho <75154388+Andiwn121@users.noreply.github.com>
2023-09-21 09:24:33 +0700

    update db + seeder

---
+++ database/migrations/2023_09_21_014302_create_mahasiswas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id('id_mahasiswa');
+            $table->string('universitas');
+            $table->string('fakultas');
+            $table->string('program_studi');
+            $table->string('nama');
+            $table->integer('telepon');
+            $table->integer('umur');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
