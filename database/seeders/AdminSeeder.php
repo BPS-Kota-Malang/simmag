0110164516f157ac22c15e8581ed51ac10761ebd
Andi Wicaksono Nugroho <75154388+Andiwn121@users.noreply.github.com>
2023-10-02 15:19:54 +0700

    update

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -18,9 +18,38 @@ class AdminSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Admin',
+            'name' => 'Admin Produksi',
             'email' => 'Admin@gmail.com',
             'role'  => 2,
+            'keterangan'  => 'Pembina Produksi',
+            'password' => Hash::make('password'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Admin Sosial',
+            'email' => 'Admin2@gmail.com',
+            'role'  => 2,
+            'keterangan'  => 'Pembina Sosial',
+            'password' => Hash::make('password'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Admin Neraca',
+            'email' => 'Admin3@gmail.com',
+            'role'  => 2,
+            'keterangan'  => 'Pembina Neraca',
+            'password' => Hash::make('password'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Admin IPDS',
+            'email' => 'Admin4@gmail.com',
+            'role'  => 2,
+            'keterangan'  => 'Pembina IPDS',
+            'password' => Hash::make('password'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Admin Distribusi',
+            'email' => 'Admin5@gmail.com',
+            'role'  => 2,
+            'keterangan'  => 'Pembina Distribusi',
             'password' => Hash::make('password'),
         ]);
     }
