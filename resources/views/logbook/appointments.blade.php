0c38e7c8eef7a6d7181d107545f0bb1082b2048e
unknown <muhamadkbar47@gmail.com>
2023-10-09 10:51:48 +0700

    Update Interface Logbook

--- resources/views/logbook/appointments.blade.php
+++ resources/views/logbook/appointments.blade.php
@@ -8,15 +8,12 @@
 
 <div class="card">
     <div class="card-header py-3 d-flex">
-        <h6 class="m-0 font-weight-bold text-primary">
-        </h6>
         <div class="ml-auto">
-            <span class="icon text-white-50">
-                <i class="fa fa-plus"></i>
-            </span>
-            <button type="button" class="btn btn-primary.mt-n1 {
-  margin-top: -0.25rem !important;
-}   ">Primary</button>
+        <a href="{{ route('logbook') }}" class="btn btn-primary">
+                <span class="icon text-white-50">
+                    <i class="fa fa-plus"></i>
+                </span>
+                <span class="text">{{ __('Buat Logbook') }}</span>
             </a>
         </div>
     </div>
@@ -41,11 +38,11 @@
                         <td>
 
                         </td>
-                        <td>Muhammad Akbar Rayyan Al Fath</td>
-                        <td>Politeknik Negeri Jember</td>
-                        <td>21 Agustus 2023</td>
-                        <td>07.30</td>
-                        <td>15.00</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                         <td>-</td>
                         <td>
                         </td>
