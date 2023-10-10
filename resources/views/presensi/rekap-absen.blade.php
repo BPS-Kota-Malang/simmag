706e3491639cedc7cddf252489736574822475fb
MohammadDiegoPutraMeiAbadi <diegoputra120602@gmail.com>
2023-10-10 21:53:47 +0700

    update tampilan presensi untuk user dan admin

--- resources/views/presensi/rekap-absen.blade.php
+++ resources/views/presensi/rekap-absen.blade.php
@@ -6,10 +6,17 @@
 <div class="container-fluid py-4">
     <div class="row mt-4 mx-4">
         <div class="col-12">
-            <div class="card mb-4">
-                <div class="card-header pb-0">
-                    <h6>Rekap Presensi</h6>
+            <div class="card mb-3">
+                <div class="card-body p-3 fw-bold text-dark d-flex justify-content-between">
+                    <div class="d-flex align-items-center">
+                        <div class="icon icon-shape bg-gradient-success shadow-success text-center rounded-circle">
+                            <i class="ni ni-collection text-lg opacity-10" aria-hidden="true"></i>
+                        </div>
+                        <xspan class="mx-3 fs-4">Rekap Presensi</xspan>
+                    </div>
                 </div>
+            </div>
+            <div class="card mb-4 p-3">
                 <div class="card-body px-0 pt-0 pb-2">
                     <div class="form-group">
                         <label for="label">Tanggal Awal</label>
@@ -22,104 +29,56 @@
                     <div class="form-group">
                         <a href="" onclick="this.href='/rekap-absen/'+ document.getElementById('tglawal').value +
                             '/' + document.getElementById('tglakhir').value " class="btn btn-primary col-md-12">
-                            Lihat <i class="fas fa-print"></i>
+                            Lihat
                         </a>
                     </div>
-                    <div class="card-body px-0 pt-0 pb-2">
-                        <div class="table-responsive p-0">
-                            <table class="table align-items-center mb-0">
-                                <thead>
-                                    <tr>
-                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama
-                                        </th>
-                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
-                                            Tanggal
-                                        </th>
-                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                            Masuk
-                                        </th>
-                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                            Keluar
-                                        </th>
-                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                            Jumlah Jam Kerja
-                                        </th>
-                                    </tr>
-                                </thead>
-                                @foreach ($presensi as $item)
-                                <tbody>
-                                    <tr>
-                                        <td>
-                                            <div class="d-flex px-3 py-1">
-                                                <div class="d-flex flex-column justify-content-center">
-                                                    <h6 class="mb-0 text-sm">{{ $item->user->name }}</h6>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <p class="text-sm font-weight-bold mb-0">{{ $item->tgl }}</p>
-                                        </td>
-                                        <td class="align-middle text-center text-sm">
-                                            <p class="text-sm font-weight-bold mb-0">{{ $item->jammasuk }}</p>
-                                        </td>
-                                        <td class="align-middle text-center text-sm">
-                                            <p class="text-sm font-weight-bold mb-0">{{ $item->jamkeluar }}</p>
-                                        </td>
-                                        <td class="align-middle text-center text-sm">
-                                            <p class="text-sm font-weight-bold mb-0">{{ $item->jamkerja }}</p>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                                <!-- <tr>
-                                        <td>{{ $item->user->name }}</td>
-                                        <td>{{ $item->tgl }}</td>
-                                        <td>{{ $item->jammasuk }}</td>
-                                        <td>{{ $item->jamkeluar }}</td>
-                                        <td>{{ $item->jamkerja }}</td>
-                                    </tr> -->
-                                @endforeach
-                            </table>
+                    <div class="card mb-4 p-3">
+                        <div class="card-body px-0 pt-0 pb-2">
+                            <div class="table-responsive p-0">
+                                <table id="example" class="table align-items-center mb-0">
+                                    <thead>
+                                        <tr>
+                                            <th class="text-center text-uppercase text-xs font-weight-bolder">Nama</th>
+                                            <th class="text-center text-uppercase text-xs font-weight-bolder">Tanggal</th>
+                                            <th class="text-center text-uppercase text-xs font-weight-bolder">Jam Masuk</th>
+                                            <th class="text-center text-uppercase text-xs font-weight-bolder">Jam Keluar</th>
+                                            <th class="text-center text-uppercase text-xs font-weight-bolder">Jam Kerja</th>
+                                            <!-- <th class="text-secondary opacity-7"></th> -->
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach($presensi as $item)
+                                        <tr>
+                                            <!-- NO -->
+                                            <td class="align-middle text-center text-sm">
+                                                {{ $item->user->name }}
+                                            </td>
+
+                                            <td class="align-middle text-center text-sm">
+                                                {{ $item->tgl }}
+                                            </td>
+
+                                            <td class="align-middle text-center text-sm">
+                                                {{ $item->jammasuk }}
+                                            </td>
+
+                                            <td class="align-middle text-center text-sm">
+                                                {{ $item->jamkeluar }}
+                                            </td>
+
+                                            <td class="align-middle text-center text-sm">
+                                                {{ $item->jamkerja }}
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<section class="py-7">
-    <div class="container">
-        <div class="row justify-space-between py-2">
-            <div class="col-lg-4 mx-auto">
-                <ul class="pagination pagination-primary m-4">
-                    <li class="page-item">
-                        <a class="page-link" href="javascript:;" aria-label="Previous">
-                            <span aria-hidden="true"><i class="fa fa-angle-double-left" aria-hidden="true"></i></span>
-                        </a>
-                    </li>
-                    <li class="page-item active">
-                        <a class="page-link" href="javascript:;">1</a>
-                    </li>
-                    <li class="page-item">
-                        <a class="page-link" href="javascript:;">2</a>
-                    </li>
-                    <li class="page-item">
-                        <a class="page-link" href="javascript:;">3</a>
-                    </li>
-                    <li class="page-item">
-                        <a class="page-link" href="javascript:;">4</a>
-                    </li>
-                    <li class="page-item">
-                        <a class="page-link" href="javascript:;">5</a>
-                    </li>
-                    <li class="page-item">
-                        <a class="page-link" href="javascript:;" aria-label="Next">
-                            <span aria-hidden="true"><i class="fa fa-angle-double-right" aria-hidden="true"></i></span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</section>
-@include('components/footer')
-@endsection
+    @include('components/footer')
+    @endsection
