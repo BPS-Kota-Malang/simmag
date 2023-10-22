5641e650d630162ad758722293cbb6a116ec306f
MohammadDiegoPutraMeiAbadi <diegoputra120602@gmail.com>
2023-10-22 10:38:05 +0700

    fitur export

--- resources/views/presensi/rekap-absen.blade.php
+++ resources/views/presensi/rekap-absen.blade.php
@@ -35,9 +35,10 @@
                     <div class="card mb-4 p-3">
                         <div class="card-body px-0 pt-0 pb-2">
                             <div class="table-responsive p-0">
-                                <table id="example" class="table align-items-center mb-0">
+                                <table id="rekap" class="table table-hover table-bordered table-stripped align-items-center">
                                     <thead>
                                         <tr>
+                                            <th class="text-center text-uppercase text-xs font-weight-bolder">No.</th>
                                             <th class="text-center text-uppercase text-xs font-weight-bolder">Nama</th>
                                             <th class="text-center text-uppercase text-xs font-weight-bolder">Tanggal</th>
                                             <th class="text-center text-uppercase text-xs font-weight-bolder">Jam Masuk</th>
@@ -46,27 +47,32 @@
                                             <!-- <th class="text-secondary opacity-7"></th> -->
                                         </tr>
                                     </thead>
+                                    @php
+                                    $count = 1;
+                                    @endphp
                                     <tbody>
                                         @foreach($presensi as $item)
                                         <tr>
                                             <!-- NO -->
-                                            <td class="align-middle text-center text-sm">
+                                            <td class="text-center align-items-center">{{ $count++ }}</td>
+
+                                            <td class="text-center align-items-center">
                                                 {{ $item->user->name }}
                                             </td>
 
-                                            <td class="align-middle text-center text-sm">
+                                            <td class="text-center align-items-center">
                                                 {{ $item->tgl }}
                                             </td>
 
-                                            <td class="align-middle text-center text-sm">
+                                            <td class="text-center align-items-center">
                                                 {{ $item->jammasuk }}
                                             </td>
 
-                                            <td class="align-middle text-center text-sm">
+                                            <td class="text-center align-items-center">
                                                 {{ $item->jamkeluar }}
                                             </td>
 
-                                            <td class="align-middle text-center text-sm">
+                                            <td class="text-center align-items-center">
                                                 {{ $item->jamkerja }}
                                             </td>
                                         </tr>
@@ -81,4 +87,67 @@
         </div>
     </div>
     @include('components/footer')
+
+    <script>
+        $('#rekap').DataTable({
+            "order": [
+                [0, 'asc']
+            ],
+            "columnDefs": [{
+                    "targets": [4],
+                    "orderable": false
+                },
+                {
+                    "targets": [0, 4],
+                    "className": "text-center"
+                },
+                {
+                    "width": "130px",
+                    "targets": 4
+                },
+            ],
+            dom: 'Bfrtip',
+            buttons: [{
+                    extend: 'pdf',
+                    text: '<i class="fa fa-file-pdf text-danger"></i> PDF',
+                    title: 'Data Divisi',
+                    exportOptions: {
+                        columns: ':visible'
+                    },
+                    messageTop: '',
+                    orientation: 'portrait',
+                    pageSize: 'A4'
+                },
+                {
+                    extend: 'excel',
+                    text: '<i class="fa fa-file-excel text-success" > </i> EXCEL',
+                    title: 'Daftar Admin',
+                    exportOptions: {
+                        columns: ':visible'
+                    },
+                    messageTop: ''
+                },
+                {
+                    extend: 'print',
+                    text: '<i class="fa fa-print text-info" > </i> PRINT',
+                    title: 'Daftar Admin',
+                    exportOptions: {
+                        columns: ':visible'
+                    },
+                    messageTop: '',
+                },
+                {
+                    extend: 'colvis',
+                    text: '<i class="fa fa-table" > </i> Columns',
+                    postfixButtons: ['colvisRestore']
+                },
+            ],
+
+            "dom": "<'row'<'col-sm-12 col-md-2'l><'col-sm-12 col-md-6'B><'col-sm-12 col-md-4'f>>" +
+                "<'row'<'col-sm-12'tr>>" +
+                "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>"
+
+        });
+    </script>
+
     @endsection
