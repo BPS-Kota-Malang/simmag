9480f756681acbfc01fb6e971ba0cfda617e953d
unknown <muhamadkbar47@gmail.com>
2023-11-04 08:34:59 +0700

    Update sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -325,7 +325,6 @@ $user = 'active';
                     </span>
                 </a>
             </li>
-            @endif
             <li class="nav-item">
                 <a class="nav-link {{ $profile }}" href="{{ route('profile.show-admin') }}">
                     <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
